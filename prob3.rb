ea59ce6eca4054f25e18559f2ec7518f17246b67
Naimah Muhammad <naimahmuhammad07@gmail.com>
2024-02-20 16:53:39 -0600

    Created User class and raise ArguementError if username empty

---
+++ prob3.rb
@@ -0,0 +1,19 @@
+class User
+attr_reader :username
+
+  def initialize(username)
+    @username = username
+  end
+
+  def username=(username)
+    raise ArgumentError, "Username cannot be empty" if
+username.to_s.strip.empty?
+    @username = username
+  end
+end
+
+user = User.new("Robot")
+puts user.username
+user.username = "Machine"
+puts user.username
+user.username = ""
